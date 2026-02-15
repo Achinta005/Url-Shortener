4ddba9bf6f43ffde9c21dc2dd9ee34ec65b9cdab
Achinta Hazra <achintahazra8515@gmail.com>
2026-02-15 22:43:44 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3002;
+
+// Enable CORS for all routes
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://appsy-ivory.vercel.app"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
